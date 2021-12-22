6f0b95c32bfe3aca70ac743787396d48c3e1d49f
Panca Aji Nugraha <78069598+panji665@users.noreply.github.com>
2021-12-22 15:34:47 +0700

    Add files via upload

--- VID 1-9/app/Views/template/header.php
+++ VID 1-9/app/Views/template/header.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Layout</title>
+    <title>Aplikasi | Restoran CI-4</title>
 </head>
 <body>
+    
 <nav>
     <ul>
         <a href="<?= base_url() ?>/admin/kategori"> <li>Select</li> </a>
         <a href="<?= base_url() ?>/admin/kategori/form"> <li>Insert</li> </a>
         <a href="<?= base_url() ?>/admin/kategori/update/5"> <li>Update</li> </a>
     </ul>
-
-    <?= $this->renderSection('content') ?>
-    
 </nav>
-</body>
-</html>
